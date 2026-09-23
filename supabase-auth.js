ae9c3f89b5f4c3294022304ff972822f56d828a2
hechbuild <niaba.voyage@gmail.com>
2026-09-23 01:52:42 +0000

    Connect Niaba Voyage frontend to Supabase Auth

---
+++ supabase-auth.js
@@ -0,0 +1,17 @@
+const NIABA_SUPABASE_URL="https://bzfvfpfbusfmfvtohrph.supabase.co";
+const NIABA_SUPABASE_KEY="sb_publishable_9YJkPMzoEfhu25VosgPzDw_hwP1_ygR";
+const niabaSupabase=window.supabase.createClient(NIABA_SUPABASE_URL,NIABA_SUPABASE_KEY);
+window.niabaSupabase=niabaSupabase;
+
+window.niabaRequireUser=async function(redirect=true){
+  const {data:{user}}=await niabaSupabase.auth.getUser();
+  if(!user&&redirect) location.href="/";
+  return user;
+};
+window.niabaLogout=async function(){await niabaSupabase.auth.signOut();location.href="/";};
+
+(async()=>{
+ const {data:{user}}=await niabaSupabase.auth.getUser();
+ document.querySelectorAll(".account-actions").forEach(el=>el.hidden=!!user);
+ document.querySelectorAll(".nav-logout").forEach(el=>el.hidden=!user);
+})();
